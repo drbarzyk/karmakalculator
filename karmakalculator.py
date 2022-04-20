8f994fd9aed8468f9a596fe5d878b7aba4b04c0e
Dan Barzyk <dbarz@Dans-MacBook-Pro.local>
2022-04-20 08:07:15 -0400

    Requested posts automatically saved in csv format

--- karmakalculator.py
+++ karmakalculator.py
@@ -1,19 +1,57 @@
 # Karma Machine
 # 4 / 20 / 22
 
+from doctest import IGNORE_EXCEPTION_DETAIL
 from dotenv import load_dotenv, set_key
 from datetime import datetime
 from dateutil import parser
+from json import loads
 import os
+import requests
+import pandas as pd
 
-def main():
-    load_dotenv()
-
+def validate_api_token():
     now = datetime.now()
     requestTimeout = 2 * 60 * 60    #2 hours
     requestDuration = now - parser.parse(os.environ.get('timestamp'))
     if requestDuration.total_seconds() > requestTimeout:
         exec(open("request.py").read())
+
+def send_request(limit=25):
+    headers = os.environ.get('headers')
+    headers = loads(headers.replace("'", '"'))
+    return requests.get("https://oauth.reddit.com/r/AskReddit/top", 
+                        headers=headers,
+                        params={'limit': f'{limit}'})
+
+def build_df(res):
+    labels = [
+        'subreddit',
+        'title',
+        'selftext',
+        'upvote_ratio',
+        'ups',
+        'downs',
+        'score',
+    ]
+
+    df = pd.DataFrame(columns=labels)
+    print(len(res.json()['data']['children']))
+    for post in res.json()['data']['children']:
+        data = {}
+        for col in df.columns:
+            data[col] = post['data'][col]
+        post_df = pd.DataFrame([data])
+        df = pd.concat([df, post_df], ignore_index=True)
+    
+    return df
+
+def main():
+    load_dotenv()
+    validate_api_token()
+    res = send_request()
+    df = build_df(res)
+    df.to_csv('reddit.csv')
 
 if __name__ == '__main__':
     main()
